8219d142d2adcdad04968397b15eb606470fc633
poornima <poornima02011999@gmail.com>
2025-07-04 20:18:27 +0530

    hai1

--- output.tf
+++ output.tf
@@ -1,9 +1,7 @@
 output "instance_ids" {
-    value = [ for instance in aws_instance.poorni_ec2 : instance.id]
-
-  
+  value = [for instance in aws_instance.poorni_ec2 : instance.id]
 }
+
 output "public_ips" {
-    value = [ for instance in aws_aws_instance.poorni_ec2 : instance.public_ips ]
-  
+  value = [for instance in aws_instance.poorni_ec2 : instance.public_ip]
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-region = "eu-west-1"
-ami = "ami-0fab1b527ffa9b942"
+region         = "eu-west-1"
+ami            = "ami-0fab1b527ffa9b942"
 instance_count = 11
-instance_type = "t2.micro"
+instance_type  = "t2.micro"
