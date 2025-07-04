691d59e1683deeee677316e61a53fdece795c655
poornima <poornima02011999@gmail.com>
2025-07-04 20:01:47 +0530

    ec2 second

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-    region = "eu-west-1"
+    region = var.region
   
 }
 resource "aws_instance" "poorni_ec2" {
-    #count = 11
+    count = var.instance_count
+    ami = var.ami
+    instance_type = var.instance_type
     #ami = "ami-0fab1b527ffa9b942"
     #instance_type = "t2.micro"
   
